56b7dfc34fad19844bd8b3f448e03f8dff697663
hrodiwulfaz <hrodiwulfaz0@gmail.com>
2025-11-17 14:54:36 +0100

    Build - Add custom version prefix

--- modules/mono/build_scripts/build_assemblies.py
+++ modules/mono/build_scripts/build_assemblies.py
@@ -288,6 +288,8 @@ def generate_sdk_package_versions():
             version_status = version_status[:pos] + "." + version_status[pos:]
         version_str += "-" + version_status
 
+    version_str += "-outpostia"
+
     import version
 
     version_defines = (
